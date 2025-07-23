935a43eea36d6e821228b213987f8a1c0a2f7692
Geekko390 <121685977+Geekko390@users.noreply.github.com>
2025-07-23 07:38:05 -0400

    Add .next/static/chunks/src_app_page_tsx_d0a9d12d._.js 1753272844361

---
+++ .next/static/chunks/src_app_page_tsx_d0a9d12d._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_afd88761._.js",
+  "static/chunks/node_modules_1fab59d5._.js"
+],
+    source: "dynamic"
+});
